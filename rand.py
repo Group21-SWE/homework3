fa11e3352166c7fd51d860473216d16520072ed0
Trent-Wiens <trentwiens135@gmail.com>
2025-02-03 17:20:40 -0500

    fixed rand.py formatting

--- rand.py
+++ rand.py
@@ -1,8 +1,23 @@
+'''
+Method for creating a random array
+'''
+
 import subprocess
 
 def random_array(arr):
+    '''
+    This take an array and randomizes it
+    
+    Parameters
+    -----
+    arr - array to be randomizzed
+    
+    Returns
+    -----
+    randomized array
+    '''
     shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
+    for i in enumerate(arr):
+        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True, check=False)
         arr[i] = int(shuffled_num.stdout)
     return arr

a49e845a40eaeea470eb68be5a3f16a9ad5d5645
Haven Brown <hbrown4@ncsu.edu>
2025-02-03 17:49:24 -0500

    fixed logic bugs in merge sort

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -1,39 +1,68 @@
 import rand
 import bubble_sort
 
-def mergeSort(arr):
-    if (len(arr) == 1):
+def merge_sort(arr):
+    if len(arr) == 1:
         return arr
 
     half = len(arr)//2
+    print("half = " + str(half))
 
-    return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
+    return recombine(merge_sort(arr[:half]), merge_sort(arr[half:]))
 
-def recombine(leftArr, rightArr):
-    leftIndex = 0
-    rightIndex = 0
-    mergeArr = [None] * (len(leftArr) + len(rightArr))
-    while leftIndex < len(leftArr) and rightIndex < len(rightArr):
-        if leftArr[leftIndex] < rightArr[rightIndex]:
-            rightIndex += 1
-            mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
-        else:
-            leftIndex += 1
-            mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
-
-    for i in range(rightIndex, len(rightArr)):
-        mergeArr[leftIndex + rightIndex] = rightArr[i]
+def recombine(left_arr, right_arr):
+    print("left arr")
+    print(left_arr)
+    print("right arr")
+    print(right_arr)
     
-    for i in range(leftIndex, len(leftArr)):
-        mergeArr[leftIndex + rightIndex] = leftArr[i]
+    if len(left_arr) == 0 and len(right_arr) == 0:
+        return
+    
+    left_index = 0
+    right_index = 0
 
-    return mergeArr
+    merge_arr = [None] * (len(left_arr) + len(right_arr))
+    while left_index < len(left_arr) and right_index < len(right_arr):
+        if left_arr[left_index] < right_arr[right_index]:
+            print(str(left_arr[left_index]) + " < " + str(right_arr[right_index]))
+            merge_arr[left_index + right_index] = left_arr[left_index]
+            left_index += 1
+        else:
+            print(str(left_arr[left_index]) + " > " + str(right_arr[right_index]))
+            merge_arr[left_index + right_index] = right_arr[right_index]
+            right_index += 1
+        print("while sorting array")
+        print(merge_arr)
+
+    for i in range(right_index, len(right_arr)):
+        print(str(left_index + right_index))
+        merge_arr[left_index + i] = right_arr[i]
+        print("iterating over remainder of right array")
+        print(merge_arr)
+
+    for i in range(left_index, len(left_arr)):
+        merge_arr[i + right_index] = left_arr[i]
+        print("iterating over remainder of left array")
+        print(merge_arr)
+        
+
+    print(merge_arr)
+    return merge_arr
 
 arr = rand.random_array([None] * 20)
-arr_out = mergeSort(arr)
+print(arr)
+lil_arr = [0,6,3,9,2,0]
+print(lil_arr)
+
+arr_out = merge_sort(lil_arr)
+print(arr_out)
+arr_out = merge_sort(arr)
 print(arr_out)
 
-arr_out = bubble_sort(arr)
+arr_out = bubble_sort.bubble_sort(lil_arr)
+print(arr_out)
+arr_out = bubble_sort.bubble_sort(arr)
 print(arr_out)
 
 
--- rand.py
+++ rand.py
@@ -3,6 +3,6 @@ import subprocess
 def random_array(arr):
     shuffled_num = None
     for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
+        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True, check=False)
         arr[i] = int(shuffled_num.stdout)
     return arr
